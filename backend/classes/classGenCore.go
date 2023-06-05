7b666418edf2f08002dd2cb56579fc485bf7cb71
vasiliy-klimov-idf <vasiliy.klimov@idfinance.com>
2023-06-06 00:03:30 +0300

    0.0.5 add class analyze

--- backend/classes/classGenCore.go
+++ backend/classes/classGenCore.go
@@ -3,6 +3,7 @@ package classes
 import (
 	"fmt"
 	"pregen/backend/dice"
+	"reflect"
 	"sort"
 )
 
@@ -58,12 +59,13 @@ func RerollClassStats() Class {
 }
 
 func GetNewClass() Class {
-	sortStatMap(RerollClassStats())
+	randClass := RerollClassStats()
+	randClass.ClassName = statAnalyze(sortStats(randClass), randClass)
 	//classAnalyzer(RerollClassStats()) //defug
-	return RerollClassStats()
+	return randClass
 }
 
-func sortStatMap(class Class) {
+func sortStats(class Class) []string {
 	var statsMap = map[string]int{
 		"Strength":       class.Strength,
 		"Dexterity":      class.Dexterity,
@@ -81,25 +83,100 @@ func sortStatMap(class Class) {
 	sort.SliceStable(keys, func(i, j int) bool {
 		return statsMap[keys[i]] > statsMap[keys[j]]
 	})
-
+	top3stats := []string{}
 	for i, k := range keys {
 		if i == 0 || i == 1 || i == 2 { //первые 3 значения
+			top3stats = append(top3stats, k)
 			fmt.Println(k, statsMap[k])
 		}
 	}
+	fmt.Println(top3stats)
+	return top3stats
+}
+func statAnalyze(stats []string, cl Class) string {
+
+	var statForMage = []string{"Intelligence", "BodyDifficulty", "Charisma"}
+	var statForMage2 = []string{"Intelligence", "Dexterity", "Charisma"}
+	var statForMage3 = []string{"Intelligence", "Wisdom", "Charisma"}
+
+	var statForFighter = []string{"Strength", "Dexterity", "Intelligence"}
+	var statForFighter2 = []string{"Strength", "Dexterity", "BodyDifficulty"}
+
+	var statForRogue = []string{"Dexterity", "Intelligence", "Charisma"}
+	var statForRogue2 = []string{"Dexterity", "Intelligence", "Wisdom"}
+
+	var statForWarrior = []string{"Strength", "BodyDifficulty", "Wisdom"}
+	var statForWarrior2 = []string{"Strength", "BodyDifficulty", "Charisma"}
+
+	var statForBard = []string{"Charisma", "Dexterity", "Wisdom"}
+	var statForBard2 = []string{"Charisma", "Dexterity", "BodyDifficulty"}
+	var statForBard3 = []string{"Charisma", "Dexterity", "Intelligence"}
+
+	var statForDruid = []string{"Wisdom", "BodyDifficulty", "Dexterity"}
+	var statForDruid2 = []string{"Wisdom", "BodyDifficulty", "Strength"}
+
+	var statForRanger = []string{"Dexterity", "Wisdom", "Charisma"}
+	var statForRanger2 = []string{"Dexterity", "Wisdom", "Strength"}
+
+	tempSortArray := [][]string{statForMage, statForMage2, statForMage3,
+		statForFighter, statForFighter2, statForRogue, statForRogue2, statForWarrior, statForWarrior2,
+		statForBard, statForBard2, statForBard3, statForDruid, statForDruid2, statForRanger, statForRanger2}
+
+	for _, arrstrings := range tempSortArray {
+		sort.Strings(arrstrings)
+	}
+	switch {
+	case reflect.DeepEqual(stats, statForMage):
+		cl.ClassName = "Def MAg"
+	case reflect.DeepEqual(stats, statForMage2):
+		cl.ClassName = "Def MAg"
+	case reflect.DeepEqual(stats, statForMage3):
+		cl.ClassName = "Def MAg"
+	case reflect.DeepEqual(stats, statForFighter):
+		cl.ClassName = "Def MAg"
+	case reflect.DeepEqual(stats, statForFighter2):
+		cl.ClassName = "Def MAg"
+	case reflect.DeepEqual(stats, statForRogue):
+		cl.ClassName = "Def MAg"
+	case reflect.DeepEqual(stats, statForRogue2):
+		cl.ClassName = "Def MAg"
+	case reflect.DeepEqual(stats, statForWarrior):
+		cl.ClassName = "Def MAg"
+	case reflect.DeepEqual(stats, statForWarrior2):
+		cl.ClassName = "Def MAg"
+	case reflect.DeepEqual(stats, statForBard):
+		cl.ClassName = "Def MAg"
+	case reflect.DeepEqual(stats, statForBard2):
+		cl.ClassName = "Def MAg"
+	case reflect.DeepEqual(stats, statForBard3):
+		cl.ClassName = "Def MAg"
+	case reflect.DeepEqual(stats, statForDruid):
+		cl.ClassName = "Def MAg"
+	case reflect.DeepEqual(stats, statForDruid2):
+		cl.ClassName = "Def MAg"
+	case reflect.DeepEqual(stats, statForRanger):
+		cl.ClassName = "Def MAg"
+	case reflect.DeepEqual(stats, statForRanger2):
+		cl.ClassName = "Def MAg"
+	default:
+		cl.ClassName = "????"
+
+	}
+
+	return cl.ClassName
 }
 
 // make for other classes metrics for generation
-func classAnalyzer(class Class) Class {
-	var sum int
-	sum = class.BodyDifficulty + class.Dexterity + class.Intelligence +
-		class.Charisma + class.Strength + class.Wisdom
-	//balancer
-	if sum < 67 || sum > 78 {
-		class = RerollClassStats()
-		sum = class.BodyDifficulty + class.Dexterity + class.Intelligence +
-			class.Charisma + class.Strength + class.Wisdom
-		//fmt.Println("Перерол на", sum)
-	}
-	return class
-}
+//func classAnalyzer(class Class) Class {
+//	var sum int
+//	sum = class.BodyDifficulty + class.Dexterity + class.Intelligence +
+//		class.Charisma + class.Strength + class.Wisdom
+//	//balancer
+//	if sum < 67 || sum > 78 {
+//		class = RerollClassStats()
+//		sum = class.BodyDifficulty + class.Dexterity + class.Intelligence +
+//			class.Charisma + class.Strength + class.Wisdom
+//		//fmt.Println("Перерол на", sum)
+//	}
+//	return class
+//}
