2872b58f049f9d5990047f18fa76af75fa802f59
vasiliy-klimov-idf <vasiliy.klimov@idfinance.com>
2023-08-24 12:40:54 +0300

    0.7.5 fix passive wisdom

--- pkg/characterCore/skillsGenCore.go
+++ pkg/characterCore/skillsGenCore.go
@@ -40,7 +40,6 @@ func SetSkillsForCharacter() Skills {
 	var sk Skills
 	// принимает нужный экземпляр обьекта и в себя же записывает новые данные
 	sk = setSkillsNames(sk)
-	sk = setSkillModifierValue(sk)
 
 	skillsSlice := classes.GetAnalyzedSkillSlice(backgrounds.BackgroundSkillMastery)
 	sk = setSkillProficiency(skillsSlice, sk)
@@ -90,89 +89,67 @@ func setSkillsNames(sk Skills) Skills {
 
 	return sk
 }
-func setSkillModifierValue(sk Skills) Skills {
-
-	sk.Athletics.ModifierValue = classes.ModifierGlobal.Strength
-	sk.Acrobatics.ModifierValue = classes.ModifierGlobal.Dexterity
-	sk.SleightOfHand.ModifierValue = classes.ModifierGlobal.Dexterity
-	sk.Stealth.ModifierValue = classes.ModifierGlobal.Dexterity
-	sk.Arcana.ModifierValue = classes.ModifierGlobal.Intelligence
-	sk.History.ModifierValue = classes.ModifierGlobal.Intelligence
-	sk.Investigation.ModifierValue = classes.ModifierGlobal.Intelligence
-	sk.Nature.ModifierValue = classes.ModifierGlobal.Intelligence
-	sk.Religion.ModifierValue = classes.ModifierGlobal.Intelligence
-	sk.AnimalHandling.ModifierValue = classes.ModifierGlobal.Wisdom
-	sk.Insight.ModifierValue = classes.ModifierGlobal.Wisdom
-	sk.Medicine.ModifierValue = classes.ModifierGlobal.Wisdom
-	sk.Perception.ModifierValue = classes.ModifierGlobal.Wisdom
-	sk.Survival.ModifierValue = classes.ModifierGlobal.Wisdom
-	sk.Deception.ModifierValue = classes.ModifierGlobal.Charisma
-	sk.Intimidation.ModifierValue = classes.ModifierGlobal.Charisma
-	sk.Performance.ModifierValue = classes.ModifierGlobal.Charisma
-	sk.Persuasion.ModifierValue = classes.ModifierGlobal.Charisma
-
-	return sk
-}
 
 func setSkillProficiency(skillsSlice []string, sk Skills) Skills {
 	for _, profSkill := range skillsSlice {
 		switch profSkill {
 		case sk.Athletics.SkillName:
 			sk.Athletics.Proficiency = true
-			sk.Athletics.ModifierValue = sk.Athletics.ModifierValue + 2
+			sk.Athletics.ModifierValue = classes.ModifierGlobal.Strength + 2
 		case sk.Acrobatics.SkillName:
 			sk.Acrobatics.Proficiency = true
-			sk.Acrobatics.ModifierValue = sk.Acrobatics.ModifierValue + 2
+			sk.Acrobatics.ModifierValue = classes.ModifierGlobal.Dexterity + 2
 		case sk.SleightOfHand.SkillName:
 			sk.SleightOfHand.Proficiency = true
-			sk.SleightOfHand.ModifierValue = sk.SleightOfHand.ModifierValue + 2
+			sk.SleightOfHand.ModifierValue = classes.ModifierGlobal.Dexterity + 2
 		case sk.Stealth.SkillName:
 			sk.Stealth.Proficiency = true
-			sk.Stealth.ModifierValue = sk.Stealth.ModifierValue + 2
+			sk.Stealth.ModifierValue = classes.ModifierGlobal.Dexterity + 2
 		case sk.Arcana.SkillName:
 			sk.Arcana.Proficiency = true
-			sk.Arcana.ModifierValue = sk.Arcana.ModifierValue + 2
+			sk.Arcana.ModifierValue = classes.ModifierGlobal.Intelligence + 2
 		case sk.History.SkillName:
 			sk.History.Proficiency = true
-			sk.History.ModifierValue = sk.History.ModifierValue + 2
+			sk.History.ModifierValue = classes.ModifierGlobal.Intelligence + 2
 		case sk.Investigation.SkillName:
 			sk.Investigation.Proficiency = true
-			sk.Investigation.ModifierValue = sk.Investigation.ModifierValue + 2
+			sk.Investigation.ModifierValue = classes.ModifierGlobal.Intelligence + 2
 		case sk.Nature.SkillName:
 			sk.Nature.Proficiency = true
-			sk.Nature.ModifierValue = sk.Nature.ModifierValue + 2
+			sk.Nature.ModifierValue = classes.ModifierGlobal.Intelligence + 2
 		case sk.Religion.SkillName:
 			sk.Religion.Proficiency = true
-			sk.Religion.ModifierValue = sk.Religion.ModifierValue + 2
+			sk.Religion.ModifierValue = classes.ModifierGlobal.Intelligence + 2
 		case sk.AnimalHandling.SkillName:
 			sk.AnimalHandling.Proficiency = true
-			sk.AnimalHandling.ModifierValue = sk.AnimalHandling.ModifierValue + 2
+			sk.AnimalHandling.ModifierValue = classes.ModifierGlobal.Wisdom + 2
 		case sk.Insight.SkillName:
 			sk.Insight.Proficiency = true
-			sk.Insight.ModifierValue = sk.Insight.ModifierValue + 2
+			sk.Insight.ModifierValue = classes.ModifierGlobal.Wisdom + 2
 		case sk.Medicine.SkillName:
 			sk.Medicine.Proficiency = true
-			sk.Medicine.ModifierValue = sk.Medicine.ModifierValue + 2
+			sk.Medicine.ModifierValue = classes.ModifierGlobal.Wisdom + 2
 		case sk.Perception.SkillName:
 			sk.Perception.Proficiency = true
-			sk.Perception.ModifierValue = sk.Perception.ModifierValue + 2
+			sk.Perception.ModifierValue = classes.ModifierGlobal.Wisdom + 2
 		case sk.Survival.SkillName:
 			sk.Survival.Proficiency = true
-			sk.Survival.ModifierValue = sk.Survival.ModifierValue + 2
+			sk.Survival.ModifierValue = classes.ModifierGlobal.Wisdom + 2
 		case sk.Deception.SkillName:
 			sk.Deception.Proficiency = true
-			sk.Deception.ModifierValue = sk.Deception.ModifierValue + 2
+			sk.Deception.ModifierValue = classes.ModifierGlobal.Charisma + 2
 		case sk.Intimidation.SkillName:
 			sk.Intimidation.Proficiency = true
-			sk.Intimidation.ModifierValue = sk.Intimidation.ModifierValue + 2
+			sk.Intimidation.ModifierValue = classes.ModifierGlobal.Charisma + 2
 		case sk.Performance.SkillName:
 			sk.Performance.Proficiency = true
-			sk.Performance.ModifierValue = sk.Performance.ModifierValue + 2
+			sk.Performance.ModifierValue = classes.ModifierGlobal.Charisma + 2
 		case sk.Persuasion.SkillName:
 			sk.Persuasion.Proficiency = true
-			sk.Persuasion.ModifierValue = sk.Persuasion.ModifierValue + 2
+			sk.Persuasion.ModifierValue = classes.ModifierGlobal.Charisma + 2
 		}
 	}
+
 	setPassiveWisdom(sk)
 	return sk
 }
