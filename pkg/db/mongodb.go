fa527c1a6599cc88f8d15a2252a644fc0fe9ceef
vasiliy-klimov-idf <vasiliy.klimov@idfinance.com>
2023-09-25 00:41:43 +0300

    0.9.2 update MongoDB read

--- pkg/db/mongodb.go
+++ pkg/db/mongodb.go
@@ -42,7 +42,7 @@ func ConnectToDB() *mongo.Client {
 	// Use the SetServerAPIOptions() method to set the Stable API version to 1
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI("mongodb://" + address + "/" + DBNAME).
-		SetServerAPIOptions(serverAPI).SetAuth(cred).SetLoadBalanced(true)
+		SetServerAPIOptions(serverAPI).SetAuth(cred)
 	// Create a new client and connect to the server
 	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
