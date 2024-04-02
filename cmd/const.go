5c92991fad34b747c25ee47af6ad49f3abddbc3b
redian23 <vasya.degski@gmail.com>
2024-04-03 00:47:26 +0300

    1.1.0.1 add yandex metric

--- cmd/const.go
+++ cmd/const.go
@@ -1,7 +1,7 @@
 package main
 
 const (
-	Version      = "1.1 Stable-build-2024-03-11"
+	Version      = "1.1 Stable-build-2024-04-03"
 	GlobPattern  = "/opt/charbox/*/*.html"
 	WebPagesPath = "/opt/charbox/web"
 	ImagePath    = "/opt/charbox/web/img"
